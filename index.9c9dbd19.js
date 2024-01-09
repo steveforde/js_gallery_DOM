bc78b4f1c7cd42ad9fb6e05c037a1ec4c3bcbee6
Stephen Forde <sforde08@gmail.com>
2024-01-09 23:01:39 +0000

    make build

---
+++ index.9c9dbd19.js
@@ -0,0 +1,3 @@
+var e=document.querySelectorAll("a"),r=document.querySelector("#largeImg").parentNode;e.forEach(function(e){return e.addEventListener("click",function(n){n.preventDefault(),r.innerHTML='\n    <img id="largeImg" src="'.concat(e.href,'"\n    alt="main image" class="gallery__large-img gallery__img">\n  ')})});//# sourceMappingURL=index.9c9dbd19.js.map
+
+//# sourceMappingURL=index.9c9dbd19.js.map
